04f05b33831221606fd69f159d57186442c31d88
Abuzar Khan <abuzarkhan1242@gmail.com>
2025-06-26 11:05:05 +0500

    starter

---
+++ src/main/java/com/abuzar/ghibli_art/GhibliArtApplication.java
@@ -0,0 +1,13 @@
+package com.abuzar.ghibli_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhibliArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhibliArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abuzar/ghibli_art/GhibliArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abuzar.ghibli_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhibliArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
